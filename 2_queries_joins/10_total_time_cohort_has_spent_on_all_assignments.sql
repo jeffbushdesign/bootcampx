0ba1dd8e82ab5c665450b7699b789985d535b2f9
Jeff Bush <jeffbushdesign@gmail.com>
2022-04-10 02:06:10 +0000

    Add join queries

---
+++ 2_queries_joins/10_total_time_cohort_has_spent_on_all_assignments.sql
@@ -0,0 +1,13 @@
+-- SELECT *
+SELECT sum(assignment_submissions.duration) as total_duration
+FROM assignment_submissions
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = 'FEB12';
+-- WHERE students.name = 'Ibrahim Schimmel';
+
+
+-- FROM assignment_submissions
+-- JOIN students ON student_id = students.id
+-- JOIN cohorts ON students.cohort_id = cohorts.id
+-- WHERE students.name = 'Ibrahim Schimmel';
